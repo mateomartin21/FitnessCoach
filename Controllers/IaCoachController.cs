8372319f6fc67e19d92a2819a113801c541fc1e5
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2026-06-26 22:41:27 -0600

    feat: interfaz para IA agregada

---
+++ Controllers/IaCoachController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCoach.Controllers
+{
+    public class IaCoachController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
